678908091b3991b38d4c67a1d0a70adeba217788
claxon007 <claportuesi@icloud.com>
2020-12-24 13:57:59 +0100

    alpha graphics done

--- COVID Info/Controllers/First Open Pages/FirstOpenLaunchPage1.swift
+++ COVID Info/Controllers/First Open Pages/FirstOpenLaunchPage1.swift
@@ -10,9 +10,14 @@ import UIKit
 class FirstOpenLaunchPage1: UIViewController {
     let currentPage = Pages.pageOne
     
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var text1: UILabel!
+    @IBOutlet weak var text2: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        titleLabel.font = UIFont.sourceCodeProBold(size: 30)
 //        print("called")
 //        view.addSubview(storyboardUI.instantiateViewController(withIdentifier: "FIRST_OPEN_PAGE_1").view)
     }
--- COVID Info/Controllers/MainScreenController.swift
+++ COVID Info/Controllers/MainScreenController.swift
@@ -53,3 +53,21 @@ class MainScreenController: UIViewController {
     }
 }
 
+extension UIFont {
+  static func euclidCircularBBold(size: CGFloat) -> UIFont {
+    return UIFont(name: "EuclidCircularB-Bold", size: size)!
+  }
+
+  static func euclidCircularBMedium(size: CGFloat) -> UIFont {
+    return UIFont(name: "EuclidCircularB-Medium", size: size)!
+  }
+
+  static func euclidCircularBSemibold(size: CGFloat) -> UIFont {
+    return UIFont(name: "EuclidCircularB-Semibold", size: size)!
+  }
+
+  static func sourceCodeProBold(size: CGFloat) -> UIFont {
+    return UIFont(name: "SourceCodePro-Bold", size: size)!
+  }
+}
+
